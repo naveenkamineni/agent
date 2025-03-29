257632d8ad23a29b78548050e7fea14bf1426dc5
naveenkamineni <110465159+naveenkamineni@users.noreply.github.com>
2025-03-29 12:02:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,4 +32,5 @@ def index():
     return render_template("index.html", response=response)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
